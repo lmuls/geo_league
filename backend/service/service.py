324897644e35b95684947f575d7cce6a476e3d56
Lars Marius Ulsnes (Stasjonær) <lm@tigne.no>
2024-04-16 21:14:50 +0200

    Removes Score(should be round)

--- backend/service/service.py
+++ backend/service/service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from database import models, schemas
-from database.models import Player, Score, Round, Game
+from database.models import Player, Score, Game
 from database.schemas import Leaderboard, PlayerResult, GameResult
 
 
@@ -40,8 +40,7 @@ def create_game(db: Session, game: schemas.GameCreate):
     return game
 
 def create_score(db: Session, score: schemas.ScoreCreate):
-    score = models.Score(round_id=score.round_id, player_id=score.player_id, lat=score.lat, lng=score.lng, timed_out=score.timed_out, timed_out_with_guess=score.timed_out_with_guess,
-                         round_score_points=score.round_score_points, round_score_percentage=score.round_score_percentage, distance_meters=score.distance_meters, time=score.time)
+    score = models.Score(round_id=score.round_id, player_id=score.player_id, score=score.score, meta=score.meta)
     db.add(score)
     db.commit()
     db.refresh(score)
@@ -52,24 +51,23 @@ def get_leaderboard(db: Session) -> Leaderboard:
     res: Leaderboard = Leaderboard(players=[])
     players: list[Player] = db.query(Player).all()
 
+    # TODO Refactor
     for player in players:
         scores = db.query(Score).filter(Score.player_id == player.id).all()
 
         points = 0
         for score in scores:
-            points += score.round_score_points
+            points += score.score
         games: list[Game] = db.query(Game).join(Round).join(Score).filter(Score.player_id == player.id).all()
 
         game_results: list[GameResult] = []
         for game in games:
-            scores: list[Score] = db.query(Score).join(Round).filter(Round.game_id == game.id).filter(Score.player_id == player.id).all()
-            rounds = []
+            scores: list[Score] = db.query(Score).filter(Score.game_id == game.id).filter(Score.player_id == player.id).all()
             game_points = 0
             for score in scores:
-                rounds.append(score)
-                game_points += score.round_score_points
+                game_points += score.score
 
-            game_results.append(GameResult(id=game.id, rounds=rounds, map_name=game.map_name, points=game_points, date=game.date))
+            game_results.append(GameResult(id=game.id, map_name=game.map_name, points=game_points, date=game.date))
         game_results.sort(key=lambda x: x.points, reverse=True)
         res.players.append(PlayerResult(name=player.name, points=points, games=game_results))
     res.players.sort(key=lambda x: x.points, reverse=True)
