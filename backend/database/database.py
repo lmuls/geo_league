8e87871cbc4205caa9e6c750bcc97b4f534ba3df
Lars Marius Ulsnes <lars.marius.ulsnes@soprasteria.com>
2025-08-21 14:40:26 +0200

    Change to dotenv for env vars

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,8 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+print(os.getenv("DB_URL"))
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URL")
 
